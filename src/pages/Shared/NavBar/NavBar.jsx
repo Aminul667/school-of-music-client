89f76c329713e95f447ce43d1b470099e674ffb9
Aminul667 <rahathssn@gmail.com>
2023-06-09 22:06:36 +0600

    added login and social login component

--- src/pages/Shared/NavBar/NavBar.jsx
+++ src/pages/Shared/NavBar/NavBar.jsx
@@ -1,50 +1,81 @@
-import { Link } from 'react-router-dom';
-import logo from '../../../assets/images/logo.png'
+import { Link } from "react-router-dom";
+import logo from "../../../assets/images/logo.png";
+import useAuth from "../../../hooks/useAuth";
 
 const NavBar = () => {
-    return (
-        <nav>
-        <div className="flex flex-wrap items-center justify-between">
-            <div className="flex justify-center items-center lg:w-[20%]">
-                <img src={logo} />
-                <div className="ml-3 text-2xl lg:pl-0">
-                    SchoolOfMusic
-                </div>
-            </div>
-            <div className="m-5 lg:hidden" id="menu-button">
-                <h2 className='text-4xl font-bold'>P</h2>
-            </div>
-            <div className="h-20 w-screen lg:w-[70%] clip-custom" id="menu">
-                <div>
-                    <div className="flex items-center justify-center h-8 border-x-0 border-colorPrimaryDark border-b-2">
-                        <ul className="flex gap-6 font-bold">
-                            <li><Link to='/'>Home</Link></li>
-                            <li><a href="#">Instructors</a></li>
-                            <li><a href="#">Classes</a></li>
-                            <li><Link to='/login'>Login</Link></li>
-                        </ul>
-                    </div>
-                    <div className="flex items-center justify-center">
-                        <ul className="flex h-12 gap-10 md:pt-[0.7rem] uppercase text-sm md:text-lg font-bold">
-                            <li><a href="#">Dashboard</a></li>
-                            <li><a href="#">career</a></li>
-                            <li><a href="#">features</a></li>
-                            <li><a href="#">gallery</a></li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-            <div className="w-[10%] hidden lg:block">
-                <div className="flex items-center justify-center">
-                    {/* <img className="w-4 h-4 mr-3 " src="/static_files/svgs/phone-flip-solid.svg" alt="phone"> */}
-                    <div className="font-bold text-black ">
-                        900 234-1234
-                    </div>
-                </div>
-            </div>
+  const { user, logOut } = useAuth();
+
+  const handleLogOut = () => {
+    logOut()
+      .then(() => {})
+      .catch((error) => console.log(error));
+  };
+
+  return (
+    <nav>
+      <div className="flex flex-wrap items-center justify-between">
+        <div className="flex justify-center items-center lg:w-[20%]">
+          <img src={logo} />
+          <div className="ml-3 text-2xl lg:pl-0">SchoolOfMusic</div>
         </div>
+        <div className="m-5 lg:hidden" id="menu-button">
+          <h2 className="text-4xl font-bold">P</h2>
+        </div>
+        <div className="h-20 w-screen lg:w-[70%] clip-custom" id="menu">
+          <div>
+            <div className="flex items-center justify-center h-8 border-x-0 border-colorPrimaryDark border-b-2">
+              <ul className="flex gap-6 font-bold">
+                <li>
+                  <Link to="/">Home</Link>
+                </li>
+                <li>
+                  <a href="#">Instructors</a>
+                </li>
+                <li>
+                  <a href="#">Classes</a>
+                </li>
+
+                {user ? (
+                  <>
+                    <button onClick={handleLogOut}>Logout</button>
+                  </>
+                ) : (
+                  <>
+                    <li>
+                      <Link to="/login">Login</Link>
+                    </li>
+                  </>
+                )}
+              </ul>
+            </div>
+            <div className="flex items-center justify-center">
+              <ul className="flex h-12 gap-10 md:pt-[0.7rem] uppercase text-sm md:text-lg font-bold">
+                <li>
+                  <a href="#">Dashboard</a>
+                </li>
+                <li>
+                  <a href="#">career</a>
+                </li>
+                <li>
+                  <a href="#">features</a>
+                </li>
+                <li>
+                  <a href="#">gallery</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div className="w-[10%] hidden lg:block">
+          <div className="avatar">
+            <div className="w-14 rounded-full">
+              <img src={user?.photoURL} title={user?.displayName} />
+            </div>
+          </div>
+        </div>
+      </div>
     </nav>
-    );
+  );
 };
 
 export default NavBar;
