36d2b0ec3db31c0e0a19a4a22f6b0f703289aed5
Aminul667 <rahathssn@gmail.com>
2023-06-13 22:07:39 +0600

    added AddClass section

--- src/pages/AddClass/AddClass.jsx
+++ src/pages/AddClass/AddClass.jsx
@@ -1,10 +1,95 @@
+import { useForm } from "react-hook-form";
+import useAxiosSecure from "../../hooks/useAxiosSecure";
+import useAuth from "../../hooks/useAuth";
 
 const AddClass = () => {
-    return (
-        <div>
-            <h2>Class will be added here</h2>
+  const { user } = useAuth();
+  console.log("Add", user);
+  const [axiosSecure] = useAxiosSecure();
+  const { register, handleSubmit, reset } = useForm();
+  //   const img_hosting_url = `https://api.imgbb.com/1/upload?key=${img_hosting_token}`;
+
+  const onSubmit = (data) => {
+    console.log('Form', data);
+  };
+
+  return (
+    <div className="w-full px-10">
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="form-control w-full mb-4">
+          <label className="label">
+            <span className="label-text font-semibold">Class Name*</span>
+          </label>
+          <input
+            type="text"
+            placeholder="Class Name"
+            {...register("className", { required: true, maxLength: 120 })}
+            className="input input-bordered w-full "
+          />
         </div>
-    );
+        <div className="form-control w-full my-4">
+          <label className="label">
+            <span className="label-text">Class Image</span>
+          </label>
+          <input
+            type="file"
+            {...register("image", { required: true })}
+            className="file-input file-input-bordered w-full "
+          />
+        </div>
+        <div className="form-control w-full mb-4">
+          <label className="label">
+            <span className="label-text font-semibold">Instructor Name</span>
+          </label>
+          <input
+            type="text"
+            placeholder="Instructor Name"
+            {...register("instructorName", { required: true, maxLength: 120 })}
+            defaultValue={user.displayName}
+            readOnly
+            className="input input-bordered w-full "
+          />
+        </div>
+        <div className="form-control w-full mb-4">
+          <label className="label">
+            <span className="label-text font-semibold">Email</span>
+          </label>
+          <input
+            type="email"
+            placeholder="Email"
+            {...register("email", { required: true, maxLength: 120 })}
+            defaultValue={user.email}
+            readOnly
+            className="input input-bordered w-full "
+          />
+        </div>
+        <div className="form-control w-full mb-4">
+          <label className="label">
+            <span className="label-text font-semibold">Available Set</span>
+          </label>
+          <input
+            type="text"
+            placeholder="Available Set"
+            {...register("availableSet", { required: true, maxLength: 120 })}
+            className="input input-bordered w-full "
+          />
+        </div>
+        <div className="form-control w-full mb-4">
+          <label className="label">
+            <span className="label-text font-semibold">Price</span>
+          </label>
+          <input
+            type="text"
+            placeholder="Price"
+            {...register("price", { required: true, maxLength: 120 })}
+            className="input input-bordered w-full "
+          />
+        </div>
+
+        <input className="btnPrimary mt-4" type="submit" value="Add Class" />
+      </form>
+    </div>
+  );
 };
 
 export default AddClass;
